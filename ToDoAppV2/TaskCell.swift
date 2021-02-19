36280a85be86011b20e47c394250ca40e3285340
Umut Gür <umutgur@outlook.com>
2021-02-19 21:23:00 +0300

    initial commit

---
+++ ToDoAppV2/TaskCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskCell.swift
+//  ToDoAppV2
+//
+//  Created by umut on 19.02.2021.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
